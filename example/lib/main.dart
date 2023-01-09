3b28110d10ae2ef7c375872d24110adcede7587e
JosephNK <nkw0608@gmail.com>
2023-01-09 09:17:12 +0900

    working..

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,23 +1,29 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_native_view/flutter_native_view.dart';
+// import 'package:flutter_native_view/flutter_native_view.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp();
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  late final WebViewController _webViewController;
+  // late final WebViewController _webViewController;
 
   @override
   void initState() {
     super.initState();
+
+    // if (Platform.isAndroid) WebView.platform = AndroidWebView();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
 
   @override
@@ -31,8 +37,14 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             children: [
               Container(color: Colors.orangeAccent, height: 50.0),
-              _buildNativeView(),
+              // _buildNativeView(),
               // _buildNativeWebView(),
+              SizedBox(
+                height: 500,
+                child: WebView(
+                  initialUrl: 'https://flutter.dev',
+                ),
+              ),
               Container(color: Colors.pinkAccent, height: 50.0),
             ],
           ),
@@ -41,22 +53,22 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  Widget _buildNativeView() {
-    return const SizedBox(
-      height: 10000, // <-- For larger sizes, a crash occurs.
-      child: NativeYellowView(),
-    );
-  }
+  // Widget _buildNativeView() {
+  //   return const SizedBox(
+  //     height: 10000, // <-- For larger sizes, a crash occurs.
+  //     child: NativeYellowView(),
+  //   );
+  // }
 
-  Widget _buildNativeWebView() {
-    return SizedBox(
-      height: 10000, // <-- For larger sizes, a crash occurs.
-      child: NativeWebView(
-        onWebViewCreated: (controller) {
-          _webViewController = controller;
-          controller.setUrl(url: 'https://flutter.dev/');
-        },
-      ),
-    );
-  }
+  // Widget _buildNativeWebView() {
+  //   return SizedBox(
+  //     height: 10000, // <-- For larger sizes, a crash occurs.
+  //     child: NativeWebView(
+  //       onWebViewCreated: (controller) {
+  //         _webViewController = controller;
+  //         controller.setUrl(url: 'https://flutter.dev/');
+  //       },
+  //     ),
+  //   );
+  // }
 }
